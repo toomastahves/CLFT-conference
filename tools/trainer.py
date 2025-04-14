789d3889487beb7651842734adf8fd226b539d1d
Toomas Tahves <toomastahves@hotmail.com>
2025-04-14 17:57:28 +0300

    fix early_stopping

--- tools/trainer.py
+++ tools/trainer.py
@@ -153,7 +153,7 @@ class Trainer(object):
             writer.close()
 
             early_stop_index = round(valid_epoch_loss, 4)
-            early_stopping(early_stop_index, epoch, self.model, optimizer=self.optimizer_clft)
+            early_stopping(early_stop_index, epoch, self.model, optimizer=self.optimizer_clft, modality=modality)
             if ((epoch + 1) % self.config['General']['save_epoch'] == 0 and epoch > 0):
                 print('Saving model for every N epochs...')
                 save_model_dict(self.config, epoch, self.model, self.optimizer_clft)
